8a737e105d28860e9076447013943de5fab5b06d
AlexandrLezhnev <103891558+AlexandrLezhnev@users.noreply.github.com>
2022-11-28 05:38:27 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './style.css';
+import Men from './Men';
+import './stylesNew.css';
+import Buttons from './Buttons'
+
+function App() {
+  return (
+      <div className="body">
+
+      <header className="text-content">
+              <Men />   
+          </header>
+
+          <Buttons />
+
+
+    </div>
+  );
+}
+
+export default App;
